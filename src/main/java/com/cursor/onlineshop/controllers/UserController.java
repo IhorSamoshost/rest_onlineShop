e989192990b52c9b4729152fee7b3d52ba7b4d7a
Taras Lavreniuk <lavrt@amazon.com>
2021-02-20 13:43:09 +0100

    Add UserController instructions

---
+++ src/main/java/com/cursor/onlineshop/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.cursor.onlineshop.controllers;
+
+public class UserController {
+
+    // login -> return JWT
+    // createUser
+
+    // getUserInfo -> return fName, lName, age, phoneNumber
+    // editUser -> edit fName, lName, age or phoneNumber
+}
