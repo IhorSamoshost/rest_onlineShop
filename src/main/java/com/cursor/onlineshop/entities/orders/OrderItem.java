9befc25834524b9df7813c01921e18918b24fcff
Dmytro <65898747+fill19@users.noreply.github.com>
2021-02-28 13:38:07 +0200

    my part of task)))

--- src/main/java/com/cursor/onlineshop/entities/orders/OrderItem.java
+++ src/main/java/com/cursor/onlineshop/entities/orders/OrderItem.java
@@ -1,12 +1,36 @@
 package com.cursor.onlineshop.entities.orders;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 // DB Entity
+@Entity
+@Table(name = "order_item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
-
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String orderItemId;
+
+    @OneToOne
+    @JoinColumn(name = "item_id")
     private String itemId;
+
+    @Column(name = "quantity")
     private int quantity;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    public OrderItem(String orderItemId) {
+        this.orderItemId = orderItemId;
+    }
 }
---
+++ src/main/java/com/cursor/onlineshop/repositories/OrderItemRepo.java
@@ -0,0 +1,12 @@
+package com.cursor.onlineshop.repositories;
+
+import com.cursor.onlineshop.entities.goods.Category;
+import com.cursor.onlineshop.entities.orders.Order;
+import com.cursor.onlineshop.entities.orders.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderItemRepo extends JpaRepository<Order, String> {
+    Optional<OrderItem> findByOrderItemId(String orderItemId);
+}
