e6997aef0bab88b4e1331252a93e6781f5c0ae40
Dmytro <65898747+fill19@users.noreply.github.com>
2021-02-28 14:17:02 +0200

    Update OrderItemRepo.java

--- src/main/java/com/cursor/onlineshop/repositories/OrderItemRepo.java
+++ src/main/java/com/cursor/onlineshop/repositories/OrderItemRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OrderItemRepo extends JpaRepository<Order, String> {
+public interface OrderItemRepo extends JpaRepository<OrderItem, String> {
     Optional<OrderItem> findByOrderItemId(String orderItemId);
 }
